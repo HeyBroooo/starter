a9a1fee1c977debf8d089f43136c19cea460c3c9
HeyBroooo <Mohitkumar4049a@gmail.com>
2025-01-25 20:34:46 +0530

    env-updated

--- src/main.js
+++ src/main.js
@@ -18,18 +18,5 @@ export default async ({ req, res, log, error }) => {
   } catch(err) {
     error("Could not list users: " + err.message);
   }
-
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
-
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+   
 };
