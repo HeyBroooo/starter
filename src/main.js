79ec4d3a12d8b565793acf1efb94f356f63b6f02
HeyBroooo <Mohitkumar4049a@gmail.com>
2025-01-25 20:46:32 +0530

    ..

--- src/main.js
+++ src/main.js
@@ -26,10 +26,5 @@ export default async ({ req, res, log, error }) => {
     return res.text("Pong");
   }
 
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+  return res(users);
 };
